b863396c028e081ab2d3e97a22301a4374cb6971
orbins <orwellj@mail.ru>
2023-10-19 13:54:03 +0300

    refactoring through OOP

--- synchronizer.py
+++ synchronizer.py
@@ -17,17 +17,19 @@ load_dotenv()
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 PASSWORD = os.getenv('PASSWORD')
+DATE_PLACEHOLDER = '01.01.2000'
 
 
-def main():
-    dir_path = 'C:/Users/Danil/Desktop/Foundation'
-    current_modified = time.ctime(
-        max(os.path.getmtime(root) for root, _, _ in os.walk(dir_path))
-    )
-    db_file = Path(__file__).cwd() / 'db.sqlite'
+class Synchronizer:
 
-    if not db_file.exists():
-        logger.info('Файл базы данных в текущей директории не найден.')
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+        self.last_modified = None
+        self.current_modified = None
+        self.db_file = None
+        self.zip_file = None
+
+    def create_db(self):
         connection = sqlite3.connect('db.sqlite')
         cursor = connection.cursor()
         cursor.execute(
@@ -38,40 +40,43 @@ def main():
         )
         connection.commit()
         cursor.execute(
-            """INSERT INTO modifies (dir_path)  VALUES (?)""",
-            (dir_path, )
+            """INSERT INTO modifies (dir_path)  VALUES (?, ?)""",
+            (self.dir_path, DATE_PLACEHOLDER)
         )
         connection.commit()
         connection.close()
         logger.info('Файл базы данных создан!')
 
-    connection = sqlite3.connect('db.sqlite')
-    cursor = connection.cursor()
-    last_modified = cursor.execute(
-        """SELECT updated_date FROM modifies WHERE dir_path = ?""",
-        (dir_path, )
-    ).fetchone()[0]
-    connection.close()
-    print(f"current updated date: {current_modified}")
-    print(f"date from db {last_modified}")
+    def get_date_from_db(self):
+        logger.info('Получения даты последнего изменения из базы данных')
+        connection = sqlite3.connect('db.sqlite')
+        cursor = connection.cursor()
+        try:
+            self.last_modified = cursor.execute(
+                """SELECT updated_date FROM modifies WHERE dir_path = ?""",
+                (self.dir_path, )
+            ).fetchone()[0]
+        except Exception as error:
+            logging.error(f'Ошибка получения даты последнего изменения из базы данных:\n{error}')
+        connection.close()
 
-    if last_modified != current_modified:
-        logger.info('Указанная директория обновлялась. Начинается процесс синхронизации')
-        content = os.walk(dir_path)
+    def make_zip(self):
+        logger.info('Начинается процесс создания и наполнения zip-архива')
+        content = os.walk(self.dir_path)
         try:
             zip_file = pyzipper.AESZipFile('Foundation.zip', 'w', encryption=pyzipper.WZ_AES)
             zip_file.pwd = bytes(PASSWORD, encoding='UTF-8')
             for root, folders, files in content:
                 for folder_name in folders:
                     absolute_path = os.path.join(root, folder_name)
-                    relative_path = absolute_path.replace(f'{dir_path}', '')
+                    relative_path = absolute_path.replace(f'{self.dir_path}', '')
                     logger.info(f'Добавление папки {absolute_path} в zip-архив')
                     zip_file.write(absolute_path, relative_path)
                     # absolute - путь к файлу для внесения в архив, relative - путь и имя внутри архива
 
                 for file_name in files:
                     absolute_path = os.path.join(root, file_name)
-                    relative_path = absolute_path.replace(f'{dir_path}', '')
+                    relative_path = absolute_path.replace(f'{self.dir_path}', '')
                     logger.info(f'Добавление {absolute_path} в zip-архив')
                     zip_file.write(absolute_path, relative_path)
             logger.info('Zip-архив успешно создан!')
@@ -87,12 +92,8 @@ def main():
         finally:
             zip_file.close()
 
-        headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': f'OAuth {ACCESS_TOKEN}'
-        }
-
+    @staticmethod
+    def get_upload_url(headers):
         params = {
             'path': 'Foundation.zip',
             'overwrite': True
@@ -104,14 +105,13 @@ def main():
             params=params,
             headers=headers
         ).json()
+        return response
 
-        upload_url = response['href']
-        zip_file = Path(__file__).cwd() / 'Foundation.zip'
-
+    def load_zip(self, upload_url, headers):
         logger.info('Загрузка архива на сервер')
-        with open(zip_file, 'rb') as file:
+        with open(self.zip_file, 'rb') as file:
             try:
-                response = requests.put(
+                requests.put(
                     upload_url,
                     files={'file': file},
                     headers=headers
@@ -119,22 +119,57 @@ def main():
             except KeyError as error:
                 logger.error(f'Ошибка загрузки файла на сервер:\n {error}')
 
-        # Декодирование архива с целью проверки работы софта
-        with pyzipper.AESZipFile('Foundation.zip') as zip_file:
-            zip_file.setpassword(bytes(PASSWORD, encoding='UTF-8'))
-            zip_file.extractall('Foundation')
-
+    def save_date_to_db(self):
         logger.info('Изменение даты обновления в базе данных')
         connection = sqlite3.connect('db.sqlite')
         cursor = connection.cursor()
         cursor.execute(
             f"""UPDATE modifies SET updated_date = ? WHERE dir_path = ?""",
-            (current_modified, dir_path)
+            (self.current_modified, self.dir_path)
         )
         connection.commit()
         connection.close()
-        logger.info('Синхронизация успешно завершена!')
+
+    def main(self):
+        self.current_modified = time.ctime(
+            max(os.path.getmtime(root) for root, _, _ in os.walk(self.dir_path))
+        )
+        self.db_file = Path(__file__).cwd() / 'db.sqlite'
+
+        if not self.db_file.exists():
+            logger.info('Файл базы данных в текущей директории не найден.')
+            self.create_db()
+
+        self.get_date_from_db()
+
+        if self.last_modified != self.current_modified:
+            logger.info('Указанная директория обновлялась. Начинается процесс синхронизации')
+            self.make_zip()
+            self.zip_file = Path(__file__).cwd() / 'Foundation.zip'
+
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': f'OAuth {ACCESS_TOKEN}'
+            }
+
+            response = self.get_upload_url(headers)
+            if response['status_code'] != 200:
+                return
+            upload_url = response['href']
+
+            if self.zip_file:
+                self.load_zip(upload_url, headers)
+
+            self.save_date_to_db()
+            logger.info('Синхронизация успешно завершена!')
 
 
 if __name__ == '__main__':
-    main()
+    instance = Synchronizer('C:/Users/Danil/Desktop/Foundation')
+    instance.main()
+
+# Декодирование архива с целью проверки работы софта
+# with pyzipper.AESZipFile('Foundation.zip') as zip_file:
+#     zip_file.setpassword(bytes(PASSWORD, encoding='UTF-8'))
+#     zip_file.extractall('Foundation')
