491f8de31b1bc7f4bdcefff9504418f45dcf7a4e
orbins <orwellj@mail.ru>
2023-10-19 12:10:26 +0300

    add logging

--- synchronizer.py
+++ synchronizer.py
@@ -1,15 +1,17 @@
 import logging
 import os
-import zipfile
 from pathlib import Path
 import sqlite3
 import time
+import zipfile
 
 import pyzipper
 from dotenv import load_dotenv
 import requests
 
+# SETTINGS
 logger = logging.getLogger(__name__)
+logger.setLevel('INFO')
 
 load_dotenv()
 
@@ -25,6 +27,7 @@ def main():
     db_file = Path(__file__).cwd() / 'db.sqlite'
 
     if not db_file.exists():
+        logger.info('Файл базы данных в текущей директории не найден.')
         connection = sqlite3.connect('db.sqlite')
         cursor = connection.cursor()
         cursor.execute(
@@ -40,6 +43,7 @@ def main():
         )
         connection.commit()
         connection.close()
+        logger.info('Файл базы данных создан!')
 
     connection = sqlite3.connect('db.sqlite')
     cursor = connection.cursor()
@@ -48,10 +52,79 @@ def main():
         (dir_path, )
     ).fetchone()[0]
     connection.close()
+    print(f"current updated date: {current_modified}")
+    print(f"date from db {last_modified}")
 
     if last_modified != current_modified:
-        print(f"current updated date: {current_modified}")
-        print(f"date from db {last_modified}")
+        logger.info('Указанная директория обновлялась. Начинается процесс синхронизации')
+        content = os.walk(dir_path)
+        try:
+            zip_file = pyzipper.AESZipFile('Foundation.zip', 'w', encryption=pyzipper.WZ_AES)
+            zip_file.pwd = bytes(PASSWORD, encoding='UTF-8')
+            for root, folders, files in content:
+                for folder_name in folders:
+                    absolute_path = os.path.join(root, folder_name)
+                    relative_path = absolute_path.replace(f'{dir_path}', '')
+                    logger.info(f'Добавление папки {absolute_path} в zip-архив')
+                    zip_file.write(absolute_path, relative_path)
+                    # absolute - путь к файлу для внесения в архив, relative - путь и имя внутри архива
+
+                for file_name in files:
+                    absolute_path = os.path.join(root, file_name)
+                    relative_path = absolute_path.replace(f'{dir_path}', '')
+                    logger.info(f'Добавление {absolute_path} в zip-архив')
+                    zip_file.write(absolute_path, relative_path)
+            logger.info('Zip-архив успешно создан!')
+        except IOError as error:
+            logger.error(f'Ошибка ввода при заполнении архива:\n{error}')
+            return
+        except OSError as error:
+            logger.error(f'Ошибка системы при заполнении архива:\n{error}')
+            return
+        except zipfile.BadZipfile as error:
+            logger.error(f'Ошибка записи в архив:\n{error}')
+            return
+        finally:
+            zip_file.close()
+
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': f'OAuth {ACCESS_TOKEN}'
+        }
+
+        params = {
+            'path': 'Foundation.zip',
+            'overwrite': True
+        }
+
+        logger.info('Получение url\'a для загрузки zip-архива')
+        response = requests.get(
+            'https://cloud-api.yandex.net/v1/disk/resources/upload',
+            params=params,
+            headers=headers
+        ).json()
+
+        upload_url = response['href']
+        zip_file = Path(__file__).cwd() / 'Foundation.zip'
+
+        logger.info('Загрузка архива на сервер')
+        with open(zip_file, 'rb') as file:
+            try:
+                response = requests.put(
+                    upload_url,
+                    files={'file': file},
+                    headers=headers
+                )
+            except KeyError as error:
+                logger.error(f'Ошибка загрузки файла на сервер:\n {error}')
+
+        # Декодирование архива с целью проверки работы софта
+        with pyzipper.AESZipFile('Foundation.zip') as zip_file:
+            zip_file.setpassword(bytes(PASSWORD, encoding='UTF-8'))
+            zip_file.extractall('Foundation')
+
+        logger.info('Изменение даты обновления в базе данных')
         connection = sqlite3.connect('db.sqlite')
         cursor = connection.cursor()
         cursor.execute(
@@ -60,67 +133,7 @@ def main():
         )
         connection.commit()
         connection.close()
-
-    content = os.walk(dir_path)
-    try:
-        zip_file = pyzipper.AESZipFile('Foundation.zip', 'w', encryption=pyzipper.WZ_AES)
-        zip_file.pwd = bytes(PASSWORD, encoding='UTF-8')
-        for root, folders, files in content:
-            for folder_name in folders:
-                absolute_path = os.path.join(root, folder_name)
-                relative_path = absolute_path.replace(f'{dir_path}', '')
-                print(f'Add {absolute_path} to archive')
-                zip_file.write(absolute_path, relative_path)
-
-            for file_name in files:
-                absolute_path = os.path.join(root, file_name)
-                relative_path = absolute_path.replace(f'{dir_path}', '')
-                print(f'Add {absolute_path} to archive')
-                zip_file.write(absolute_path, relative_path)
-
-        print('zip file created successfully')
-    except IOError as error:
-        print(error)
-    except OSError as error:
-        print(error)
-    except zipfile.BadZipfile as error:
-        print(error)
-    finally:
-        zip_file.close()
-
-    headers = {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': f'OAuth {ACCESS_TOKEN}'
-    }
-
-    params = {
-        'path': 'Foundation.zip',
-        'overwrite': True
-    }
-
-    # Запрос урла для загрузки
-    response = requests.get(
-        'https://cloud-api.yandex.net/v1/disk/resources/upload',
-        params=params,
-        headers=headers
-    ).json()
-    upload_url = response['href']
-    # открытие файла и его загрузка на сервер
-
-    zip_file = Path(__file__).cwd() / 'Foundation.zip'
-    with open(zip_file, 'rb') as file:
-        try:
-            response = requests.put(
-                upload_url,
-                files={'file': file},
-                headers=headers
-            )
-        except KeyError:
-            print(response.text)
-    with pyzipper.AESZipFile('Foundation.zip') as zip_file:
-        zip_file.setpassword(bytes(PASSWORD, encoding='UTF-8'))
-        zip_file.extractall('Foundation')
+        logger.info('Синхронизация успешно завершена!')
 
 
 if __name__ == '__main__':
