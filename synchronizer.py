e0fda8a1c44fe7fd1977fa82d1464a458dc37f98
orbins <orwellj@mail.ru>
2023-11-13 14:11:49 +0300

    add importer

--- synchronizer.py
+++ synchronizer.py
@@ -31,8 +31,14 @@ ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 PASSWORD = os.getenv('PASSWORD')
 DATE_PLACEHOLDER = '01.01.2000'
 
+HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': f'OAuth {ACCESS_TOKEN}'
+}
 
-class Synchronizer:
+
+class Loader:
     """
     Класс для создания архива
     на основе передаваемого пути
@@ -122,7 +128,7 @@ class Synchronizer:
             logger.error(f'Не удалось создать zip-архив. Ошибка записи в архив:\n{error}')
 
     @staticmethod
-    def get_upload_url(headers):
+    def get_upload_url():
         """
         Получение url'a для загрузки zip-архива на яндекс диск
         :param headers: заголовки к запросу
@@ -137,11 +143,11 @@ class Synchronizer:
         response = requests.get(
             'https://cloud-api.yandex.net/v1/disk/resources/upload',
             params=params,
-            headers=headers
+            headers=HEADERS
         ).json()
         return response
 
-    def load_zip(self, upload_url, headers):
+    def load_zip(self, upload_url):
         """Загрузка архива в облако"""
         logger.info('Загрузка архива на сервер')
         with open(self.zip_file, 'rb') as file:
@@ -149,7 +155,7 @@ class Synchronizer:
                 requests.put(
                     upload_url,
                     files={'file': file},
-                    headers=headers
+                    headers=HEADERS
                 )
                 return True
             except KeyError as error:
@@ -181,15 +187,10 @@ class Synchronizer:
                 logger.info('Указанная директория обновлялась. Начинается процесс синхронизации')
                 self.make_zip()
                 if self.zip_file.exists():
-                    headers = {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': f'OAuth {ACCESS_TOKEN}'
-                    }
-                    response = self.get_upload_url(headers)
+                    response = self.get_upload_url()
                     upload_url = response.get('href', None)
                     if upload_url:
-                        is_loaded = self.load_zip(upload_url, headers)
+                        is_loaded = self.load_zip(upload_url)
                         if is_loaded:
                             self.save_date_to_db()
                     else:
@@ -198,11 +199,56 @@ class Synchronizer:
                 logger.info('Указанная директория не обновлялась с последней проверки, синхронизация не требуется.')
 
 
-if __name__ == '__main__':
-    instance = Synchronizer('C:/Users/Danil/Desktop/Foundation')
-    instance.main()
+class Importer:
+    """Класс для скачивания и декодирования архива из облака"""
 
-# Декодирование архива с целью проверки работы софта
-# with pyzipper.AESZipFile('Foundation.zip') as zip_file:
-#     zip_file.setpassword(bytes(PASSWORD, encoding='UTF-8'))
-#     zip_file.extractall('Foundation')
+    @staticmethod
+    def get_upload_url():
+        """
+        Получение url'a для скачивания zip-архива с яндекс диска
+        :param headers: заголовки к запросу
+        :return:
+        """
+        params = {
+            'path': 'Foundation.zip',
+        }
+
+        logger.info('Получение url\'a для скачивания zip-архива')
+        response = requests.get(
+            'https://cloud-api.yandex.net/v1/disk/resources/download',
+            params=params,
+            headers=HEADERS
+        ).json()
+        return response
+
+    def main(self):
+        response = self.get_upload_url()
+        download_url = response.get('href', None)
+        if download_url:
+            logger.info('URL для скачивания архива получен')
+            response = requests.get(download_url, HEADERS)
+            with open('Foundation.zip', 'wb') as file:
+                file.write(response.content)
+            logger.info('Архив сохранен')
+            with pyzipper.AESZipFile('Foundation.zip') as zip_file:
+                zip_file.setpassword(bytes(PASSWORD, encoding='UTF-8'))
+                zip_file.extractall('Foundation')
+            logger.info('Архив декодирован и распакован')
+        else:
+            logger.error(f'Ошибка при полуении url\'а для скачивания:\n{response}')
+
+
+if __name__ == '__main__':
+    def select_action():
+        action = input("Введите 1, если хотите загрузить архив в облако и 2, если хотите скачать его из облака: ")
+        if action == '1':
+            instance = Loader('C:/Users/Danil/Desktop/Foundation')
+            instance.main()
+        elif action == '2':
+            instance = Importer()
+            instance.main()
+        else:
+            print('Некорректный ввод, попробуйте ещё раз!')
+            select_action()
+
+    select_action()
