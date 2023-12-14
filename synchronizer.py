43b917277aae9eb01d142517e414614eeb9fc062
griezly <daniltk.yandex.ru@mail.ru>
2023-12-14 17:21:16 +0300

    fix error

--- synchronizer.py
+++ synchronizer.py
@@ -199,7 +199,7 @@ class Loader(BaseClass):
                         is_loaded = self.load_zip(upload_url)
                         if is_loaded:
                             self.save_date_to_db()
-                            os.remove(Path(__file__).cwd() / '.zip')
+                            os.remove(Path(__file__).cwd() / f'{self.DIR_NAME}.zip')
                             logger.info('Zip-архив удалён!')
                     else:
                         logging.error(f'Ошибка получения ссылки url\'a для загрузки:\n{response}')
